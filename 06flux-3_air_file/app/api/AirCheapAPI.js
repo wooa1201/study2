5862b1d54aa14a11bf72d48707917002c5326097
DuckheeLee <shldhee@yahoo.co.kr>
2017-08-21 00:49:20 +0900

    air+_file

---
+++ 06flux-3_air_file/app/api/AirCheapAPI.js
@@ -0,0 +1,30 @@
+import 'whatwg-fetch';
+import AirportActionCreators from '../actions/AirportActionCreators';
+
+let AirCheapAPI = {
+  fetchAirports() {
+    fetch('airports.json')
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        AirportActionCreators.fetchAirportsSuccess(responseData);
+      })
+      .catch(error => {
+        AirportActionCreators.fetchAirportsError(error);
+      });
+  },
+
+  fetchTickets(origin, destination) {
+    fetch('flight.json')
+      .then(response => response.json())
+      .then(responseData => {
+        AirportActionCreators.fetchTicketsSuccess(responseData);
+      })
+      .catch(error => {
+        AirportActionCreators.fetchTicketsError(error);
+      });
+  },
+};
+
+export default AirCheapAPI;
